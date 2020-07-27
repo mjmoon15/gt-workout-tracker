ad1e326fd2792cc52cf9286ad3857e4e0ab4c25e
Mark Mooney <mjmoon15@gmail.com>
2020-07-26 22:35:13 -0400

    worked on api and view routes

--- routes/view.js
+++ routes/view.js
@@ -1,14 +1,18 @@
 const path = require("path");
 const router = require("express").Router()
 
+
+//get home/index page
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
+//get exercise page
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/exercise.html"))
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
+//get stats page
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
