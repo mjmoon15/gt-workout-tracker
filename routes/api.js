e6c359aa8bac5e977b068ae58ea9ee899fd8c4f0
Mark Mooney <mjmoon15@gmail.com>
2020-07-27 21:06:06 -0400

    working on heroku

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout.js");
+const Workout = require("../models");
 
 //find all workouts
 router.get("/api/workouts", (req, res) => {
